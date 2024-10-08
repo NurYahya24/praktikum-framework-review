7a0c9859c255466d3cbf6a87887703711bd17990
NurYahya24 <2yahyanur4@gmail.com>
2024-10-08 12:29:23 +0800

    Review 5

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.homepage),
     path('student/', views.student_index, name='student_index'),
     path('student/create/', views.student_create, name='student_create'),
+    path('student/update/<int:student_id>/', views.student_update, name='student_update'),
+    path('student/delete/<int:student_id>', views.student_delete, name='student_delete'),
 ]
